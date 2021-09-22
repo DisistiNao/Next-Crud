b8cdece593135780c2d3cf6e930c71ee1bb64dcd
DisistiNao <gabrielsaldanha373@gmail.com>
2021-09-22 14:00:23 -0300

    Layout e Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+
 export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 20, '1'),
+    new Cliente('Bia', 22, '2'),
+    new Cliente('Carol', 24, '3'),
+    new Cliente('Diana', 26, '4')
+  ]
+  
   return (
     <div className={`
-      flex h-screen justify-center items-center 
-      bg-gradient-to-r from-purple-500 to-blue-600
+      flex justify-center items-center h-screen 
+      bg-gradient-to-r from-blue-500 to-purple-500 
+      text-white
     `}>
-      <span className="text-4xl">Texto</span>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
     </div>
   )
 }

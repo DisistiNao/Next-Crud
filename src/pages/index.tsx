f1b0fafe4ba6fe24144325429147510ced66b630
DisistiNao <gabrielsaldanha373@gmail.com>
2021-09-24 12:21:10 -0300

    Botao e Formulario

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -25,11 +27,15 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
         <Tabela 
           clientes={clientes} 
           clienteSelecionado={clienteSelecionado} 
           clienteExcluido={clienteExcluido}
         ></Tabela>
+        <Formulario cliente={clientes[0]}/>
       </Layout>
     </div>
   )
